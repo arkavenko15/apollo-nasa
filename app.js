4c521b3c08a45e68683eeb89daac779093ec888d
Liuda Arkavenko <arkavenko18@gmail.com>
2021-05-16 17:03:56 +0300

    test commit

---
+++ app.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    var marquee = $('.hashtag-scroller');
+    marquee.each(function() {
+        var mar = $(this),indent = mar.width();
+        mar.marquee = function() {
+            indent--;
+            mar.css('text-indent',indent);
+            if (indent < -1 * mar.children('.hash-items').width()) {
+                indent = mar.width();
+            }
+        };
+        mar.data('interval',setInterval(mar.marquee,1000/140));
+    });
+    });
+
+$(function() {
+ 
+        $(window).scroll(function() {
+         
+        if($(this).scrollTop() != 0) {
+         
+        $('.myBtn').fadeIn();
+         
+        } else {
+         
+        $('.myBtn').fadeOut();
+         
+        }
+         
+        });
+         
+        $('.myBtn').click(function() {
+         
+        $('body,html').animate({scrollTop:0},2000);
+         
+        });
+         
+}); 
+
+
+let switchButton = document.querySelector(".switch-btn");
+let sliderRoundLeft = document.querySelector(".slider-round-left");
+let body = document.querySelector("main")
+switchButton.addEventListener('click',function(){
+    if(sliderRoundLeft.classList.contains("slider-round-left"))
+    {
+        sliderRoundLeft.classList.remove("slider-round-left")
+        sliderRoundLeft.classList.add("slider-round-right")
+        body.setAttribute("style", "background-color:silver; ");
+    }
+    else
+    {
+        sliderRoundLeft.classList.add("slider-round-left");
+        sliderRoundLeft.classList.remove("slider-round-right");
+        body.setAttribute("style", "background-color:#4b4b4b; ");
+    }
+})
